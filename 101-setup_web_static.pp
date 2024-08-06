6f45499df67ed2fa48fadb4b21cc0689bb7e3c35
Unimke1990 <agimunimke1990@gmail.com>
2024-08-06 11:46:15 +0100

    updated

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,4 +1,4 @@
-# puppet manifest preparing a server for static content deployment
+# puppet manifest preparing a server for the static content deployment
 exec { 'sudo apt-get-update':
   command => '/usr/bin/env apt-get -y update',
 }
